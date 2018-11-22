48c1f46ce42d5eba809a8cc4c05a86f00c7b3e84
tlstls <15201467352@163.com>
2018-11-22 12:31:25 +0800

    fix

--- src/Contracts/DoNewsPusher.php
+++ src/Contracts/DoNewsPusher.php
@@ -12,8 +12,8 @@ interface DoNewsPusher
 
    public static function setDeviceToken($platform, $app_id, $device_id, $deviceToken);
 
-   public static function getDeviceToken($app_id, $list_name, $page = 1, $pageSize = 100)
+   public static function getDeviceToken($app_id, $list_name, $page = 1, $pageSize = 100);
 
-   public static function getListLen($app_id, $list_name)
+   public static function getListLen($app_id, $list_name);
 
 }
